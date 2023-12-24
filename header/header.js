ac8aac918d2dfba4feb6c0281210c352650d5cbd
liammx <liammx03@gmial.com>
2023-12-24 21:31:18 +0500

    supporter

--- header/header.js
+++ header/header.js
@@ -2,3 +2,7 @@ const button = document.querySelector('.button')
 button.addEventListener('click' , () => {
     location.href = '../2ndpage/2nd.html'
 })
+const button2 = document.querySelector('.button2')
+button2.addEventListener('click' , () => {
+    location.href = '../3rd page/3rd.html'
+})
